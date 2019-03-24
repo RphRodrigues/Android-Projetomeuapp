7323ce30e9a49c00e5d1d4c471bb2bfbf83eedfb
Raphael Rodrigues de Oliveira <rodrigues.rph@gmail.com>
2019-03-24 18:06:05 -0300

    Ligando a MainActivity as demais activitys

--- app/src/main/java/com/rtstudio/projetomeuapp/MainActivity.java
+++ app/src/main/java/com/rtstudio/projetomeuapp/MainActivity.java
@@ -1,14 +1,17 @@
 package com.rtstudio.projetomeuapp;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
     private ImageView cadastrarServico;
+    private ImageView servicoAberto;
+    private ImageView servicoFechado;
+    private ImageView assumirOS;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,11 +19,35 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         cadastrarServico = findViewById(R.id.cadastrarServicoId);
+        servicoAberto = findViewById(R.id.servicosAbertosId);
+        servicoFechado = findViewById(R.id.servicosFechadosId);
+        assumirOS = findViewById(R.id.assumirOsId);
 
         cadastrarServico.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this, CadastrarServicoActivity.class));
+            }
+        });
+
+        servicoAberto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, ServicoAberto.class));
+            }
+        });
+
+        servicoFechado.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, ServicoFechado.class));
+            }
+        });
+
+        assumirOS.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, AssumirOS.class));
             }
         });
     }
