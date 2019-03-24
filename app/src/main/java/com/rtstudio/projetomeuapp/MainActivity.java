b1818cf6610a0ecc2f1770a3184df4eab55f06d3
Raphael Rodrigues de Oliveira <rodrigues.rph@gmail.com>
2019-03-24 01:19:05 -0300

    Iniciado a programação da MainActivity

--- app/src/main/java/com/rtstudio/projetomeuapp/MainActivity.java
+++ app/src/main/java/com/rtstudio/projetomeuapp/MainActivity.java
@@ -1,13 +1,27 @@
 package com.rtstudio.projetomeuapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView cadastrarServico;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        cadastrarServico = findViewById(R.id.cadastrarServicoId);
+
+        cadastrarServico.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, CadastrarServicoActivity.class));
+            }
+        });
     }
 }
