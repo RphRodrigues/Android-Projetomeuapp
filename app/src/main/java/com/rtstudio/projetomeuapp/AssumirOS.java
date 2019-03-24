a3d73ef8dadee4688801c6b8077dd55e64c89812
Raphael Rodrigues de Oliveira <rodrigues.rph@gmail.com>
2019-03-24 18:01:49 -0300

    Criação das demais activity da versão atual do aplicativo

---
+++ app/src/main/java/com/rtstudio/projetomeuapp/AssumirOS.java
@@ -0,0 +1,13 @@
+package com.rtstudio.projetomeuapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AssumirOS extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_assumir_os);
+    }
+}
---
+++ app/src/main/java/com/rtstudio/projetomeuapp/ServicoFechado.java
@@ -0,0 +1,13 @@
+package com.rtstudio.projetomeuapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ServicoFechado extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_servico_fechado);
+    }
+}
