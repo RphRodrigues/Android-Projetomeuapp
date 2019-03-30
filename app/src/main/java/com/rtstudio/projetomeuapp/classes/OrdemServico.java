ab5722659b876c272471c2b2c3d020561c4ac042
Raphael Rodrigues de Oliveira <rodrigues.rph@gmail.com>
2019-03-30 09:00:26 -0300

    Adicionado função gerarId() na classe OrdemServico para gerar um número aleatório para o campo ordemServicoId

--- app/src/main/java/com/rtstudio/projetomeuapp/classes/OrdemServico.java
+++ app/src/main/java/com/rtstudio/projetomeuapp/classes/OrdemServico.java
@@ -1,5 +1,7 @@
 package com.rtstudio.projetomeuapp.classes;
 
+import java.util.Random;
+
 public class OrdemServico {
     private int ordemServicoId;
     private Cliente cliente;
@@ -7,9 +9,14 @@ public class OrdemServico {
     private Endereco endereco;
 
     public OrdemServico(Cliente cliente, Endereco endereco, String descricaoServico) {
+        this.ordemServicoId = gerarId();
         this.cliente = cliente;
         this.descricaoServico = descricaoServico;
         this.endereco = endereco;
+    }
+
+    private int gerarId() {
+        return new Random().nextInt(1000) + 100;
     }
 
     public int getOrdemServicoId() {
