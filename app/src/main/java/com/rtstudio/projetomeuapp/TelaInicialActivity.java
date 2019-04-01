ccd4a826e3e258923edded00a5c01169e5223df7
Raphael Rodrigues de Oliveira <rodrigues.rph@gmail.com>
2019-04-01 00:42:33 -0300

    Alterado o layout na tentativa de fazer funcionar o CardView

--- app/src/main/java/com/rtstudio/projetomeuapp/TelaInicialActivity.java
+++ app/src/main/java/com/rtstudio/projetomeuapp/TelaInicialActivity.java
@@ -1,27 +1,45 @@
 package com.rtstudio.projetomeuapp;
 
-import android.content.Intent;
-import android.support.design.widget.FloatingActionButton;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+
+import com.rtstudio.projetomeuapp.adapter.CustomListAdapter;
+import com.rtstudio.projetomeuapp.classes.Card;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class TelaInicialActivity extends AppCompatActivity {
 
-    private FloatingActionButton fab;
+//    private FloatingActionButton fab;
+
+    private ListView listView;
 
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tela_inicial);
+        setContentView(R.layout.listview_layout);
 
-        fab = findViewById(R.id.telaInicial_fabId);
+        listView = findViewById(R.id.listview);
 
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(TelaInicialActivity.this, CadastrarServicoActivity.class));
-            }
-        });
+        List<Card> lista = new ArrayList<>();
+        lista.add(new Card(12345, "Instalação internet", "Raphael Rodrigues"));
+        lista.add(new Card(14698, "Reparo", "Thainan"));
+        lista.add(new Card(4569, "Manutenção"));
+        lista.add(new Card(4569, "Manutenção"));
+
+        CustomListAdapter adapter = new CustomListAdapter(this, R.layout.activity_tela_inicial, lista);
+
+        listView.setAdapter(adapter);
+//        fab = findViewById(R.id.telaInicial_fabId);
+//
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                startActivity(new Intent(TelaInicialActivity.this, CadastrarServicoActivity.class));
+//            }
+//        });
     }
 }
