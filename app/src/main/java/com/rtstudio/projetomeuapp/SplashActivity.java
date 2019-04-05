5d96f10ea7b662c245ca5d4858bb26ac94ea4515
Raphael Rodrigues de Oliveira <rodrigues.rph@gmail.com>
2019-04-04 22:15:40 -0300

    Adicionado splash screen ao aplicativo

---
+++ app/src/main/java/com/rtstudio/projetomeuapp/SplashActivity.java
@@ -0,0 +1,28 @@
+package com.rtstudio.projetomeuapp;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.app.WindowDecorActionBar;
+import android.view.WindowManager;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        getSupportActionBar().hide();
+//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), TelaInicialActivity.class));
+                finish();
+            }
+        }, 3000);
+    }
+}
