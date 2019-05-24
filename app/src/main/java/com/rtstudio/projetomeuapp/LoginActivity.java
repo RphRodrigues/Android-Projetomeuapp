aba73e7271e15190a2dfb98001db42f12b608b8d
Raphael Rodrigues de Oliveira <rodrigues.rph@gmail.com>
2019-05-24 18:38:14 -0300

    Continuidade da tela de Login, adicionado função para lembrar o login do usuário

--- app/src/main/java/com/rtstudio/projetomeuapp/LoginActivity.java
+++ app/src/main/java/com/rtstudio/projetomeuapp/LoginActivity.java
@@ -4,20 +4,24 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Switch;
 import android.widget.Toast;
 
 import com.google.gson.Gson;
-import com.rtstudio.projetomeuapp.DAO.UsuarioDAO;
 import com.rtstudio.projetomeuapp.classes.Utilitaria;
+import com.rtstudio.projetomeuapp.dao.UsuarioDAO;
 import com.rtstudio.projetomeuapp.modelo.Usuario;
+import com.rtstudio.projetomeuapp.preferencias.PreferenciasUsuario;
 
 public class LoginActivity extends AppCompatActivity {
 
     private EditText mPrimeiroNome;
     private EditText mEmail;
-    private EditText mTelefone;
     private Usuario mUsuario;
+    private Button mButtonOk;
+    private Switch mSwitch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,15 +30,22 @@ public class LoginActivity extends AppCompatActivity {
 
         mPrimeiroNome = findViewById(R.id.login_nome);
         mEmail = findViewById(R.id.login_email);
-        mTelefone = findViewById(R.id.login_telefone);
+        mButtonOk = findViewById(R.id.login_btnOk);
+        mSwitch = findViewById(R.id.login_switch);
 
-        findViewById(R.id.login_btnOk).setOnClickListener(new View.OnClickListener() {
+        if (PreferenciasUsuario.Companion.getPreferenciaLogin(this)) {
+            mSwitch.setChecked(true);
+            mPrimeiroNome.setText(PreferenciasUsuario.Companion.getNomeLogin(this));
+            mEmail.setText(PreferenciasUsuario.Companion.getEmailLogin(this));
+        } else {
+            mSwitch.setChecked(false);
+        }
+
+        mButtonOk.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 if (mPrimeiroNome.getText().toString().isEmpty() ||
-                        mEmail.getText().toString().isEmpty() ||
-                        mTelefone.getText().toString().isEmpty()) {
+                        mEmail.getText().toString().isEmpty()) {
                     new Utilitaria(LoginActivity.this).toast("Preencha todos os campos", Toast.LENGTH_SHORT);
                     return;
                 }
@@ -42,11 +53,19 @@ public class LoginActivity extends AppCompatActivity {
                 if ((mUsuario = new UsuarioDAO(getBaseContext()).getUsuarioByEmail(mEmail.getText().toString())) == null) {
                     mUsuario = new Usuario(
                             mPrimeiroNome.getText().toString(),
-                            mEmail.getText().toString(),
-                            mTelefone.getText().toString()
+                            mEmail.getText().toString()
                     );
-
                     new UsuarioDAO(getBaseContext()).insertUsuario(mUsuario);
+                }
+
+                if (mSwitch.isChecked()) {
+                    PreferenciasUsuario.Companion.setPreferenciaLogin(
+                            getBaseContext(),
+                            mPrimeiroNome.getText().toString().trim(),
+                            mEmail.getText().toString().trim(),
+                            true);
+                } else {
+                    PreferenciasUsuario.Companion.removePreferenciaLogin(getBaseContext(), false);
                 }
 
                 Intent intent = new Intent(LoginActivity.this, TelaInicialActivity.class);
